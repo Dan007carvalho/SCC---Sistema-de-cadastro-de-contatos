8993d1549169ebcdf5477fe97c8d6e08276d380b
Dan007 <dan007@hotmail.com.br>
2023-03-19 19:18:25 -0300

    Subindo o sistema SCC pela primeira vez.

---
+++ Migrations/20230225012857_criandoColunaDataCadastroEAtualizar.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CadastroContatos.Migrations
+{
+    public partial class criandoColunaDataCadastroEAtualizar : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Contatos_Usuarios_UsuarioId",
+                table: "Contatos");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "UsuarioId",
+                table: "Contatos",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DataAtualizacao",
+                table: "Contatos",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DataCadastro",
+                table: "Contatos",
+                nullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Contatos_Usuarios_UsuarioId",
+                table: "Contatos",
+                column: "UsuarioId",
+                principalTable: "Usuarios",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Contatos_Usuarios_UsuarioId",
+                table: "Contatos");
+
+            migrationBuilder.DropColumn(
+                name: "DataAtualizacao",
+                table: "Contatos");
+
+            migrationBuilder.DropColumn(
+                name: "DataCadastro",
+                table: "Contatos");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "UsuarioId",
+                table: "Contatos",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int));
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Contatos_Usuarios_UsuarioId",
+                table: "Contatos",
+                column: "UsuarioId",
+                principalTable: "Usuarios",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
